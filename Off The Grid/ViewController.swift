3b7782b93dd546dd0062617cb0c6105a8766329c
Lynn Ngo <thaoly.ngo@gmail.com>
2021-03-17 10:23:16 -0400

    Initial Commit

---
+++ Off The Grid/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Off The Grid
+//
+//  Created by Lynn Ngo on 3/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
